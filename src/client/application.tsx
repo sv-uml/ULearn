340ea21d36059ed96a077146b0e747e3a79de6a9
Saurabh Verma <17792518+sv-uml@users.noreply.github.com>
2018-10-12 22:38:01 -0500

    [Week 3] Fix package versions

--- src/client/application.tsx
+++ src/client/application.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "./layout/App"
+import { App } from "./layout/App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
 import { AppReducer } from "./reducers/AppReducer";
-
-let logger = createLogger();
 
 let localState: Object;
 if (localStorage.getItem("appState"))  {
